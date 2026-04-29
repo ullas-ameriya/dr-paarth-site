5ae9745479ba915f259f8905f49cdda380a50ed6
Ullas Ameriya <ullas.ameriya@gmail.com>
2026-04-29 17:00:52 +0530

    Changed Reviews

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,12 +1,12 @@
 import { Star, StarHalf } from "lucide-react";
 const reviews = [
   {
-    name: "Akshay Yadav",
-    text: "Best surgeon i have seen in Delhi. He replaced both my hips and knees, I have become more younger than I was 🙌🙌",
+    name: "Vaishali Chaudhary",
+    text: "Dr parth narula is very good doctor. He did my mother knee replacement surgery. My mother had very good recovery and she started walking next day only. Sir is very soft spoken and he is very good ortho surgeon. Highly recommended to everyone.",
   },
   {
-    name: "Trips Anshul",
-    text: "I got my knee replaced by him. I was running a marathon next day 🔥🔥",
+    name: "Sudesh Kadyan",
+    text: "Dr Parth Narula is a dedicated and knowledgable doctor who takes care of knowing the reason behind your ailment before jumping to a hurried conclusion. Cheerful and smiling, very politely starts his task with his observations and set of questions to get into the root cause of the problem- which could be as simple as selecting your shoes for walk very carefully. Must visit him for expert suggestions and treatment. Booking online facility also available for convenience",
   },
 ];
 
@@ -45,11 +45,11 @@ const Reviews = () => {
         </div>
 
         <a
-          href="https://www.practo.com/bangalore/doctor/dr-thimmappa-hegde-neurosurgeon"
+          href="https://www.google.com/maps/place/Narula+Ortho+%26+Physio+Centre/@28.6769803,77.2787863,17z/data=!4m8!3m7!1s0x390cfdf4d747b331:0x923edc950143c0b0!8m2!3d28.6769756!4d77.2813666!9m1!1b1!16s%2Fg%2F11myn6b_8c?hl=en-GB&entry=ttu&g_ep=EgoyMDI2MDQyNi4wIKXMDSoASAFQAw%3D%3D"
           target="_blank"
           className="inline-block mt-8 text-blue-600"
         >
-          View all reviews on Practo
+          View all reviews on Google Reviews
         </a>
 
       </div>
